497c3e1677ba8cc8d704cff41c6e78b0f95ba97d
Lia Abbott <julia@teamcmp.com>
2015-03-16 11:57:27 -0400

    fixing js error

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var mathNum = Math.sin((document.body.scrollTop / 1250);
+  var bodyNum = (document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
-    var phase = mathNum + (i % 5));
+    var phase = Math.sin( bodyNum + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
